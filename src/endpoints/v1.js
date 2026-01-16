becef8359405f56ef6d2ac9648440b2508f80620
Lucas <lukevdm64@gmail.com>
2026-01-16 08:56:50 -0500

    added console log and change the tls secret in hopes it works

--- src/endpoints/v1.js
+++ src/endpoints/v1.js
@@ -1,9 +1,10 @@
-import express from 'express';
+import express from "express";
 
 var router = express.Router();
 
-router.get('/', async function (req, res, next) {
-    return res.json({});
+router.get("/", async function (req, res, next) {
+  return res.json({ message: "made it to /" });
 });
 
 export default router;
+
